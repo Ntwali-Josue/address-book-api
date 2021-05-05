49fc06ef153bcf34627a7a21541d2110e1c45352
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-05-05 23:13:41 +0200

    add new routes for contacts crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,24 @@
-module.exports = app => {
-    const contacts = require("../controllers/index");
-  
-    // Create a new Contact
-    app.post("/contacts", contacts.create);
-  
-    // Retrieve all Contacts
-    app.get("/contacts", contacts.findAll);
-  
-    // Retrieve a single Contact with contactId
-    app.get("/contact/:contactId", contacts.findOne);
-  
-    // Update a Contact with contactId
-    app.put("/contact/:contactId", contacts.update);
-  
-    // Delete a Contact with contactId
-    app.delete("/contact/:contactId", contacts.delete);
-  
-    // Create a new Contact
-    app.delete("/contacts", contacts.deleteAll);
-};
+import { Router } from 'express';
+import {createContact, updateContact, findAllContacts, findOneContact, deleteAllContacts, deleteContact} from "../controllers/index.js"
+
+const router = Router()
+
+// Create a new Contact
+router.post("/contacts", createContact);
+
+// Retrieve all Contacts
+router.get("/contacts", findAllContacts);
+
+// Retrieve a single Contact with contactId
+router.get("/contact/:contactId", findOneContact);
+
+// Update a Contact with contactId
+router.patch("/contact/:contactId", updateContact);
+
+// Delete a Contact with contactId
+router.delete("/contact/:contactId", deleteContact);
+
+// Create a new Contact
+router.delete("/contacts", deleteAllContacts);
+
+export default router;
